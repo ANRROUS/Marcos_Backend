2ce1108fd290b49e19fe55525493be5296d5df98
Anrrous <arthuropipa@gmail.com>
2024-12-05 23:27:11 -0500

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://ANRROUS:JhQq145CSyKiLPgO@cluster0.keey7.mongodb.net/miBaseDeDatos?retryWrites=true&w=majority");
+        console.log(">>>Base de datos conectada<<<");
+    } catch (error) {
+        console.error("Error de conexión a MongoDB:", error);
+    }
+}
